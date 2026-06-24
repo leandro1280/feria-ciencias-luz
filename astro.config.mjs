2718f4291650b6229e16d475353ed8ffb4bc8bf2
leandro <sirleo1280@gmail.com>
2026-06-24 08:42:40 -0300

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://leandro.github.io', // Reemplazar con el nombre de usuario/org real
+  base: '/feria-ciencias-luz', // Reemplazar con el nombre del repositorio real
+});
